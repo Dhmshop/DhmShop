7e534abe43f0e3ac0d06ed2ac67386653e627fea
XiaoYuan0228 <2137373481@qq.com>
2020-06-02 17:44:50 +0800

    6.2_xiaoyuan

--- app/src/main/java/dhm/com/dhmshop/view/login/LoginActivity.java
+++ app/src/main/java/dhm/com/dhmshop/view/login/LoginActivity.java
@@ -3,6 +3,8 @@ package dhm.com.dhmshop.view.login;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import dhm.com.dhmshop.R;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
