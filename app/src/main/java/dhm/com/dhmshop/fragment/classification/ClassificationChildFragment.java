cbd22d45fca84dbc83caffb91e211ccfc6ef26fb
XiaoYuan0228 <2137373481@qq.com>
2020-06-09 11:23:52 +0800

    6..9

--- app/src/main/java/dhm/com/dhmshop/fragment/classification/ClassificationChildFragment.java
+++ app/src/main/java/dhm/com/dhmshop/fragment/classification/ClassificationChildFragment.java
@@ -3,17 +3,31 @@ package dhm.com.dhmshop.fragment.classification;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import dhm.com.dhmshop.R;
+import dhm.com.dhmshop.adapter.classification.ClassificationItemAdapter;
+import dhm.com.dhmshop.base.BaseFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ClassificationChildFragment extends Fragment {
+public class ClassificationChildFragment extends BaseFragment implements View.OnClickListener {
 
+
+    private View view;
+    /**
+     * 热门分类
+     */
+    private TextView mTitleClassificationchild;
+    private RecyclerView mRecyclerviewClassificationchild;
 
     public ClassificationChildFragment() {
         // Required empty public constructor
@@ -21,10 +35,52 @@ public class ClassificationChildFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_classification_child, container, false);
+    protected int getLayout() {
+        return R.layout.fragment_classification_child;
     }
 
+    @Override
+    protected void initView(View view) {
+
+        mTitleClassificationchild = (TextView) view.findViewById(R.id.title_classificationchild);
+        mRecyclerviewClassificationchild = (RecyclerView) view.findViewById(R.id.recyclerview_classificationchild);
+        mRecyclerviewClassificationchild.setOnClickListener(this);
+
+
+        ArrayList<String> titles = new ArrayList<>();
+        titles.add("时尚套装");
+        titles.add("半身裙");
+        titles.add("套装裙");
+        titles.add("辣条");
+        titles.add("女装");
+        titles.add("牛仔裤女");
+        ArrayList<Integer> images = new ArrayList<>();
+        images.add(R.drawable.home_true);
+        images.add(R.drawable.home_false);
+        images.add(R.drawable.classification_true);
+        images.add(R.drawable.classification_false);
+        images.add(R.drawable.shopcart_true);
+        images.add(R.drawable.shopcart_false);
+        ClassificationItemAdapter classificationItemAdapter = new ClassificationItemAdapter(getActivity(), titles, images);
+
+        mRecyclerviewClassificationchild.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        mRecyclerviewClassificationchild.setAdapter(classificationItemAdapter);
+
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            default:
+                break;
+            case R.id.recyclerview_classificationchild:
+                break;
+        }
+    }
 }
